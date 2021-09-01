1795154e5ac22e3c4aaaee4581179a526777c9f8
James <ball.j.work@gmail.com>
2021-09-01 15:08:38 +0100

    project filters added, minior visual enhancements

--- Static/Javascript/script.js
+++ Static/Javascript/script.js
@@ -1,4 +1,5 @@
 // add event listeners to flip cards
+
 let cards = document.getElementsByClassName('tech');
 
 for (const card of cards) {
@@ -22,7 +23,7 @@ let showCards = document.getElementById('showCards');
 
 showCards.addEventListener('click', () => {
     for (const card of cards) {
-        if (card.classList.contains('is-flipped') &  (!(card.classList.contains('show-logo'))))
+        if (card.classList.contains('is-flipped') & (!(card.classList.contains('show-logo'))))
         card.classList.toggle('is-flipped');        
     };
 });
@@ -32,12 +33,43 @@ showCards.addEventListener('click', () => {
 let hamburgerIcon = document.getElementById('hamburger')
 let mobileMenu = document.getElementById('mobile-menu');
 
+// open mobile menu
+
 hamburgerIcon.addEventListener('click', () => {
     mobileMenu.style.display = "block";
 });
+// close mobile menu
 
 let closeButton = document.getElementById('closebtn');
 
 closeButton.addEventListener('click', () => {
     mobileMenu.style.display = "none";
+});
+// close mobile menu when anchor tags clicked
+
+let mobileAnchors = document.getElementsByClassName('mobile-anchor')
+
+for (const link of mobileAnchors) {
+    link.addEventListener('click', () => {
+        mobileMenu.style.display = "none";
+    })
+};
+
+// filter project cards functions 
+
+function filterProjects (filter) {
+    let projectList = document.getElementsByClassName('project-card');
+
+    for (const project of projectList) {
+        let classStr = project.className;
+        if (filter == "showAll") project.style.display = 'flex';
+        else if (classStr.indexOf(filter) > -1) project.style.display ='flex';
+        else project.style.display = 'none';
+    };
+ };
+
+// create button event handerlers
+arrBtn = ['showAll', 'javascript', 'python', 'fullstack'];
+arrBtn.forEach( button => {
+    document.getElementById(button).addEventListener('click', () => filterProjects(button));
 });
